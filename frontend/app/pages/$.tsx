3b550e7459fa81a3c5d01840b56bf45f9b587043
Bruno Santos <bms.santos@outlook.pt>
2025-09-25 12:36:40 +0100

    feat(frontend): migrate from react-router to Next.js

--- frontend/app/pages/$.tsx
+++
@@ -1,7 +0,0 @@
-const data = (_: any, status: number) => {
-    return new Response(null, { status });
-};
-
-export async function loader() {
-  return data({}, 404);
-}
--- frontend/app/pages/_index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function About() {
-  return (
-    <div style={{ padding: "2rem" }}>
-      <h1>Home Page</h1>
-    </div>
-  );
-}
